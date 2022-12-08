cc571ab85b5f294d2970d6e83c5582c672d2cfb4
Josevs <josevs.junior@gmail.com>
2022-12-08 17:26:31 -0300

    HTML e CSS para TECNICO-LIST

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -6,10 +9,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent implements OnInit {
+  
+  ELEMENT_DATA : Tecnico[] = [
+    {
+      id: 1,
+      nome: 'valdir cezar',
+      cpf: '123.456.789-10',
+      senha: '1234',
+      email: 'valdir@email.com',
+      perfis: [0],
+      dataCriacao: '08/12/2022'
+    },
+    {
+      id: 2,
+      nome: 'valdir cezar',
+      cpf: '123.456.789-10',
+      senha: '1234',
+      email: 'valdir@email.com',
+      perfis: [0],
+      dataCriacao: '08/12/2022'
+    },
+    {
+      id: 3,
+      nome: 'valdir cezar',
+      cpf: '123.456.789-10',
+      senha: '1234',
+      email: 'valdir@email.com',
+      perfis: [0],
+      dataCriacao: '08/12/2022'
+    },
+    {
+      id: 4,
+      nome: 'valdir cezar',
+      cpf: '123.456.789-10',
+      senha: '1234',
+      email: 'valdir@email.com',
+      perfis: [0],
+      dataCriacao: '08/12/2022'
+    },
+    {
+      id: 5,
+      nome: 'valdir cezar',
+      cpf: '123.456.789-10',
+      senha: '1234',
+      email: 'valdir@email.com',
+      perfis: [0],
+      dataCriacao: '08/12/2022'
+    },
+    {
+      id: 6,
+      nome: 'valdir cezar',
+      cpf: '123.456.789-10',
+      senha: '1234',
+      email: 'valdir@email.com',
+      perfis: [0],
+      dataCriacao: '08/12/2022'
+    },
+    {
+      id: 7,
+      nome: 'valdir cezar',
+      cpf: '123.456.789-10',
+      senha: '1234',
+      email: 'valdir@email.com',
+      perfis: [0],
+      dataCriacao: '08/12/2022'
+    },        
+  ];
 
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol' , 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  } 
+
 }
+
+
+
---
+++ src/app/models/tecnico.ts
@@ -0,0 +1,10 @@
+export interface Tecnico {
+   id?: any;
+   nome: string;
+   cpf: string;
+   senha: string;
+   email: string;
+   perfis: number[];
+   dataCriacao: any;
+
+}
